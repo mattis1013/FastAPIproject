c25589cf3d0ad9a3f2210752cf095cfa4e1cac86
Mattis Valmorin <mattis.valmorin94@gmail.com>
2026-04-28 14:55:51 +0200

    Génétation d'un JWT token  pour un user, il a son badge !

--- blog/routers/authenti.py
+++ blog/routers/authenti.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-from .. import database, schemas, models
+from .. import database, schemas, models, token
 from ..hashing import Hash
 from sqlalchemy.orm import Session
+
 router = APIRouter(tags=['Authentification'])
 
 
@@ -14,4 +15,5 @@ def login(request: schemas.Login, db : Session= Depends(database.get_db)):
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid password')
     # generate a JWT TOKEN and return it
-    return user
+    access_token = token.create_access_token(data={"sub": user.email})
+    return {"access_token" : access_token, "token_type": "bearer"}
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -6,13 +6,6 @@ from ..hashing import Hash
 router = APIRouter(tags=['Users'])
 
 
-
-
-
-
-
-
-
 @router.post('/user', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
     new_user = models.User(name=request.name, email=request.email,password=Hash.bcrypt(request.password))
